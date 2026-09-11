b6938b6e415c55f8616e0b15b7dccf0f16d02501
koisadam <koisadam@fit.cvut.cz>
2026-09-11 11:53:21 +0200

    added: database schemas, sqlmodel

---
+++ schemas/portfolio.py
@@ -0,0 +1,38 @@
+from datetime import datetime, timezone
+from sqlmodel import SQLModel, Field, Relationship
+from enums.portfolio import PortfolioType, Currency
+
+
+class PortfolioBase(SQLModel):
+    name: str = Field(index=True)
+    description: str | None = Field(default=None)
+    type: PortfolioType = Field(default=PortfolioType.MIXED)
+    currency: Currency = Field(default=Currency.USD)
+    is_active: bool = Field(default=True)
+
+class Portfolio(PortfolioBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), sa_column_kwargs={"onupdate": lambda: datetime.now(timezone.utc)})
+
+    transactions: list["Transaction"] = Relationship(back_populates="portfolio", cascade_delete=True)
+
+class PortfolioCreate(PortfolioBase):
+    pass
+
+
+class PortfolioUpdate(SQLModel):
+    name: str | None = None
+    description: str | None = None
+    type: PortfolioType | None = None
+    currency: Currency | None = None
+    is_active: bool | None = None
+
+
+class PortfolioPublic(PortfolioBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
+class PortfolioWithTransactions(PortfolioPublic):
+    transactions: list["TransactionPublic"] = []
---
+++ schemas/transaction.py
@@ -0,0 +1,29 @@
+from datetime import datetime, timezone
+from sqlmodel import SQLModel, Field, Relationship
+
+class TransactionBase(SQLModel):
+    portfolio_id: int = Field(index=True, foreign_key="portfolio.id", ondelete="CASCADE")
+    ticker: str = Field(index=True, max_length=20)
+    quantity: float = Field(index=True, gt=0)
+    price: float = Field(index=True)
+
+class Transaction(TransactionBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), sa_column_kwargs={"onupdate": lambda: datetime.now(timezone.utc)})
+    portfolio: "Portfolio" = Relationship(back_populates="transactions")
+
+class TransactionCreate(TransactionBase):
+    pass
+
+
+class TransactionUpdate(SQLModel):
+    ticker: str | None = None
+    quantity: float | None = None
+    price: float | None = None
+
+
+class TransactionPublic(TransactionBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime
