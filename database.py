fae2f3436f4023efddafbac9f7541f4822fed39c
koisadam <koisadam@fit.cvut.cz>
2026-09-11 11:53:21 +0200

    added: database connection

---
+++ database.py
@@ -0,0 +1,19 @@
+from typing import Annotated
+from fastapi import Depends
+from sqlmodel import create_engine, Session, SQLModel
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+
+def create_db_and_tables() -> None:
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDependency = Annotated[Session, Depends(get_session)]
